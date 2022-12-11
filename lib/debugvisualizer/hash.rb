8ea8edcda631a406d4a433ca58f80c685a112a30
Naoto Ono <onoto1998@gmail.com>
2022-12-11 22:56:47 +0900

    Show Hash objects vertically in the plot view

--- lib/debugvisualizer/hash.rb
+++ lib/debugvisualizer/hash.rb
@@ -2,11 +2,29 @@ module DebugVisualizer
   DebugVisualizer.register do |data|
     if data.is_a?(Hash)
       {
-        id: "hash_visualizer",
+        id: "hash_as_table",
         name: "Hash As Table",
         data: {
           kind: { table: true },
           rows: data.map{|key, val| {key: key, value: val}}
+        }
+      }
+    end
+  end
+
+  DebugVisualizer.register do |data|
+    if data.is_a?(Hash)
+      {
+        id: "hash_as_plot",
+        name: "Hash As Plot",
+        data: {
+          kind: { plotly: true },
+          data: [
+            {
+              x: data.keys,
+              y: data.values
+            }
+          ]
         }
       }
     end
